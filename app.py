8b28bd7f76a9dd14748a8658b5665b77e92d9741
lookmhen <lookmhen@gmail.com>
2023-08-15 15:49:42 +0700

    Changed to WSGI(Waintress)

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, request, send_file, session
 from PyPDF2 import PdfMerger
+from waitress import serve
+import socket
 import os
 import uuid
 import secrets
 import time
+import logging
 
 # Define the maximum lifetime of a temporary file in seconds
 MAX_LIFETIME = 120  # 2 min
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
-app.config['UPLOAD_FOLDER'] = 'temp'
+tempdir=app.config['UPLOAD_FOLDER'] = 'temp'
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # #--------Configure SSL context
 # cert_dir = '/path/to/cert_directory'
@@ -26,8 +32,22 @@ app.config['UPLOAD_FOLDER'] = 'temp'
 def home():
     return render_template('index.html')
 
+def ensure_temp_directory_exists():
+    """
+    Ensure the temporary directory exists.
+    """
+    if not os.path.exists(tempdir):
+        os.makedirs(tempdir)
+
+
+
 @app.route('/merge', methods=['POST'])
 def merge():
+    
+    """
+    Merge uploaded PDF files and return the merged PDF.
+    """
+    
     uploaded_files = request.files.getlist("files[]")
 
     if not uploaded_files:
@@ -40,15 +60,13 @@ def merge():
         session_id = secrets.token_hex(16)  # Generate a unique session ID
         session['session_id'] = session_id  # Store session ID in Flask session
 
-        # Create the temp directory if it doesn't exist
-        if not os.path.exists(app.config['UPLOAD_FOLDER']):
-            os.makedirs(app.config['UPLOAD_FOLDER'])
+        ensure_temp_directory_exists() # Ensure temp directory exists
 
         # Save uploaded files to the temp directory with unique names
         for file in uploaded_files:
             if file.filename.endswith(".pdf"):
                 filename = str(uuid.uuid4()) + ".pdf"
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file_path = os.path.join(tempdir, filename)
                 file.save(file_path)
                 temp_files.append(file_path)
                 merger.append(file_path)
@@ -57,7 +75,7 @@ def merge():
             return "No PDF files found in the selected files for merging."
 
         output_filename = session['session_id'] + ".pdf"  # Use session ID in the output filename
-        output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
+        output_path = os.path.join(tempdir, output_filename)
         merger.write(output_path)
 
         return send_file(output_path, as_attachment=True, download_name=output_filename)
@@ -73,8 +91,8 @@ def merge():
                     os.remove(file_path)
 
             # Remove the temp directory if it's empty
-            if os.path.exists(app.config['UPLOAD_FOLDER']) and not os.listdir(app.config['UPLOAD_FOLDER']):
-                os.rmdir(app.config['UPLOAD_FOLDER'])
+            if os.path.exists(tempdir) and not os.listdir(tempdir):
+                os.rmdir(tempdir)
 
         except Exception as cleanup_error:
             # Handle exceptions that might occur during cleanup
@@ -82,22 +100,53 @@ def merge():
 
 # Define a cleanup function
 def cleanup_temp_files():
+    
+    """
+    Remove expired temporary files.
+    
+    """
+    
     current_time = time.time()
-
-    for file_name in os.listdir(app.config['UPLOAD_FOLDER']):
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    
+    for file_name in os.listdir(tempdir):
+        file_path = os.path.join(tempdir, file_name)
         file_age = current_time - os.path.getctime(file_path)
 
         if file_age > MAX_LIFETIME:
             try:
                 os.remove(file_path)
+                logger.info("Removed expired file: %s", file_path)
             except Exception as e:
-                print("Error removing file:", e)
+                logger.error("Error removing file: %s", e)
 
 # Run the cleanup function periodically
 @app.before_request
 def run_cleanup():
     cleanup_temp_files()
 
+def start_server():
+    """
+    Start the Waitress server.
+    """
+    host = socket.gethostbyname(socket.gethostname())
+    port = int(os.environ.get('PORT', 8080))
+
+    ensure_temp_directory_exists()
+
+    logger.info("Starting the server on %s:%d", host, port)
+    serve(app, host=host, port=port)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    start_server()
+
+# Configuration for Gunicorn (not use for now next large scale)
+# Gunicorn: The number of worker processes and concurrency is configurable, allowing you to fine-tune its behavior based on your application's requirements.
+
+#     host = os.environ.get('HOST', '0.0.0.0')
+#     port = int(os.environ.get('PORT', 8080))
+#     workers = int(os.environ.get('WORKERS', 4))
+#     worker_class = os.environ.get('WORKER_CLASS', 'sync')
+
+#     # Start Gunicorn with your app
+#     cmd = f"gunicorn -w {workers} -b {host}:{port} -k {worker_class} app:app"
+#     os.system(cmd)
